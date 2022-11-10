9ab2ecb39b29078abd8934ea58f96aac250648aa
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2022-11-10 13:15:18 +0600

    added contact section

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -6,26 +6,26 @@ import { Button, Label, Textarea, TextInput } from "flowbite-react";
 const Contact = () => {
 
   return (
-    <div>
+    <div className="mx-5 pb-10">
       <h3 className="text-center text-xl md:text-3xl font-semibold underline mt-28 mb-16">Contact</h3>
-      <div className="flex w-full">
-        <div className="flex-1">
+      <div className="sm:flex w-full">
+        <div className="flex-1 mb-12 sm:mb-0">
           <Link className="flex items-center gap-4">
             <img className="w-14" src={logo} alt="" />
             <span className="font-semibold text-3xl">SK Consultancy</span>
           </Link>
-          <div className="flex items-center gap-2 mt-5">
+          <div className="flex items-center gap-2 mt-5 pl-10 sm:pl-0">
             <MdLocationPin className="text-3xl" />
             <div>
               <p className="">Panchdona, Narsingdi</p>
               <p>Bangladesh</p>
             </div>
           </div>
-          <div className="flex gap-2 mt-12">
+          <div className="flex gap-2 mt-6 pl-10 sm:pl-0">
             <MdPhone className="text-2xl" />
             <a href="tel:+8801828008748">+8801828008748</a>
           </div>
-          <div className="flex gap-2 mt-2">
+          <div className="flex gap-2 mt-1 pl-10 sm:pl-0">
             <MdEmail className="text-2xl" />
             <a href="mail:hemon.hasan123@gmail.com">hemon.hasan123@gmail.com</a>
           </div>
